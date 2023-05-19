89bdeec5d9412731522f2b5f6600dccbbcea7a9c
Shabicha Sureshkumar <shabichasureshkumar@gmail.com>
2023-05-19 16:48:49 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Shabicha Sureshkumar on 2023-05-19.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
